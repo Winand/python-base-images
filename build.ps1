bfef3a0daa1645c1f5e1c309bdda199540a35032
Makarov Andrey <winandfx@mail.ru>
2026-07-06 17:55:46 +0300

    Dockerfile name always starts with "python-"

--- build.ps1
+++ build.ps1
@@ -3,13 +3,13 @@ param (
     [switch]$push
 )
 if ($tag -match '(?<name>.+):(?<ver>[\d.]+)-(?<libc>\w+)') {
-    $name = $Matches.name
+    # $name = $Matches.name
     $ver = $Matches.ver
     $libc = $Matches.libc
 } else {
     throw "$tag does not match 'NAME:VERSION-LIBC'"
 }
 
-$dockerfile="$name-$libc.Dockerfile"
+$dockerfile="python-$libc.Dockerfile"
 docker buildx build --build-arg PYTHON_VERSION=$ver -t $tag `
                     $(if ($push) {'--push'}) -f $dockerfile .
